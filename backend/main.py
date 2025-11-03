5f028f2e28a3be6066af3339dfc106dc558954f9
caiodeaguiar <caio.de.aguiar@hotmail.com>
2025-11-02 21:29:28 -0300

    add main backend

---
+++ backend/main.py
@@ -0,0 +1,23 @@
+from typing import Union
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Login(BaseModel):
+    username: str
+    password: str
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+@app.put("/Login")
+def authenticate(login: Login):
+    return {"oi"}
